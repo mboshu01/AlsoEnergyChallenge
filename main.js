9ab9582b3b24a586194f9ecfd9b5fa6347079849
Mike Boshuyzen <mboshu@gmail.com>
2017-10-18 19:14:06 -0400

    Javascript Basics
    Completed the javascripts basics portion of the coding challenge

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
 /*****************************************************************************************
 * Part 2
 ****************************************************************************************/
+(function() {
+
 var employees = [
         {first: "Amanda", last: "Byron", group: "Sales"},
         {first: "Ye", last: "Xia", group: "Receiving", nameOrder: "reverse"},
         {first: "Miltiades", last: "Crescens", group: "Sales"},
+        {first: "Michael", last: "Boshuyzen", group: "Development"}
         /*...don't foget to account for other entries of the same form, but with different group names.....*/
     ];
 
 // Part 2 Answer Here
 
+function sortEmployees(employees)
+{
+	var result = [];
+
+	for(var i = 0; i < employees.length; i++)
+	{
+		var employee = employees[i];
+		var name = employee.nameOrder == "reverse" ? employee.last + " " + employee.first : employee.first + " " + employee.last;
+
+		if (result.hasOwnProperty(employee.group))
+		{
+			result[employee.group].push({"name" : name});
+		} 
+		else 
+		{
+			result[employee.group] = [{"name" : name}];
+		}
+	}
+
+	return result;
+}
+
+console.log(sortEmployees(employees));
+
+})();
 /*****************************************************************************************
 * Bonus
 ****************************************************************************************/
